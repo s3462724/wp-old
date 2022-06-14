7fbec87973eefec176bfdbda3456dc4f4a0c367e
jordan3462724 <64622131+jordan3462724@users.noreply.github.com>
2022-06-14 17:46:14 +0800

    Footer Update for booking.php

--- a2/booking.php
+++ a2/booking.php
@@ -30,7 +30,7 @@
     <footer>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      </script> Jordan Kerever - s3462724 - Group B (Sama). Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
