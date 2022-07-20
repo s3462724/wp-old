b1e3675858ea5fab7567797b46fe41d7017cfc0e
jordan3462724 <64622131+jordan3462724@users.noreply.github.com>
2022-07-20 22:08:41 +0800

    a2

--- a2/booking.php
+++ a2/booking.php
@@ -1,51 +1,155 @@
-<!DOCTYPE html>
-<html lang='en'>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Lunardo Booking Page</title>
-    
-    <!-- Keep wireframe.css for debugging, add your css to style.css -->
-    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
-    <script src='../wireframe.js'></script>
-  </head>
+<html>
+<head>
+<link rel='stylesheet' href='style.css' type='text/css' />
+<link rel='stylesheet' href='flip.css' type='text/css' />
+<link rel='stylesheet' href='responsive.css' type='text/css' />
+<script>
 
-  <body>
+</script>
+<noscript> ... helpful message for those without javascript enabled ... </noscript>
+</head>
+<body>
 
-    <header>
-      <div>Put company logo and name here</div>
-    </header>
-
+<header>
+    <img src=lunardo-logo.png><h1>Lunardo</h1>
+</header>
+<div class=navigation>
     <nav>
-      <div>Put website navigation links here</div>
+        <a href="#seats">Seat Prices</a>
+        <a href="#weekly-session-times">Weekly Session Times</a>
+        <a href="#about">About Us</a>
     </nav>
+</div>
 
-    <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
-    </main>
-    <footer>
-      <div>&copy;<script>
-        document.write(new Date().getFullYear());
-      </script> Jordan Kerever - s3462724 - Group B (Sama). My github repo: <a href=https://github.com/jordan3462724/wp>https://github.com/jordan3462724/wp</a> Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
-      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
-    </footer>
-    <aside id="debug">
-      <hr>
-      <h3>Debug Area</h3>
-      <pre>
-GET Contains:
-<?php print_r($_GET) ?>
-POST Contains:
-<?php print_r($_POST) ?>
-SESSION Contains:
-<?php print_r($_SESSION) ?>
-      </pre>
-    </aside>
+<main class="main-class">
+<section>
+<h2 id="about">About Us</h2>
+    <p>- The cinema has reopened after extensive improvements and renovations.</p>
+    <p>- There are new seats: standard seats and reclinable first class seats.</p>
+    <p>- The projection and sound systems are upgraded with 3D Dolby Vision projection and Dolby Atmos sound.</p>
+    <p>- See <a href="https://protect-au.mimecast.com/s/Ovs2C5QPyyHw09LmsztJYA?domain=dolby.com">https://protect-au.mimecast.com/s/Ovs2C5QPyyHw09LmsztJYA?domain=dolby.com</a>  for more information.</p>
+</section>
+<section>
+<br>
+<h2 id="seats">Seats and Prices</h2>
 
-  </body>
+<div class="seat-div">
+    <div style="margin-left: 20%">
+        <img src=Profern-Standard-Twin.png width="500px">
+        <p>Standard Seats</p>
+    </div>
+    <div>
+        <img src=Profern-Verona-Twin.png width="400px">
+        <p>First Class Seats</p>
+    </div>
+</div>
+<br>
+<h3>Seat Prices</h3>
+<center>
+    <table>
+        <tr>
+           <th>Seat Type</th>
+           <th>Seat Code</th>
+           <th>Discounted Prices</th>
+           <th>Normal Prices</th>
+       </tr>
+       <tr>
+           <td>Standard Adult</td>
+           <td>STA</td>
+           <td>15.00</td>
+           <td>20.50</td>
+       </tr>
+      <tr>
+           <td>Standard Concession</td>
+           <td>STP</td>
+           <td>13.50</td>
+           <td>18.00</td>
+       </tr>
+      <tr>
+           <td>Standard Child</td>
+           <td>STC</td>
+           <td>12.00</td>
+           <td>16.50</td>
+       </tr>
+      <tr>
+           <td>First Class Adult</td>
+           <td>FCA</td>
+           <td>24.00</td>
+           <td>30.00</td>
+       </tr>
+      <tr>
+           <td>First Class Concession</td>
+           <td>FCP</td>
+           <td>22.50</td>
+           <td>27.00</td>
+       </tr>
+     <tr>
+           <td>First Class Child</td>
+           <td>FCC</td>
+           <td>21.00</td>
+           <td>24.00</td>
+     </tr>
+     </table>
+</center>
+</section>
+<section>
+
+    <br>
+<h2>Now Showing</h2>
+<div id='now-showing' style="margin-left: 10%">
+    <div class='card3D'>
+        <div><img width="160" height="270" id=top-gun src=top-gun-maverick-poster.jpg>
+            <h2>Top Gun: Maverick - M</h2>
+        </div>
+        <div>
+            <p>After more than thirty years of service as one of the Navy's top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.</p>
+            <p>Mon-Tue: 9pm</p>
+            <p>Wed-Fri: 9pm</p>
+            <p>Sat-Sun: 6pm</p>
+            <a type="button" href="booking.php?movie=ACT"><button>Book Now</button></a>
+        </div>
+    </div>
+    <div class='card3D'>
+        <div><img width="160" height="270" src=mrs-harris-goes-to-paris-poster.jpg>
+            <h2>Mrs. Harris Goes to Paris - PG</h2>
+        </div>
+        <div>
+            <p>A widowed cleaning lady in 1950s London falls madly in love with a couture Dior dress, and decides that she must have one of her own.</p>
+            <p>Wed-Fri: 12pm</p>
+            <p>Sat-Sun: 3pm</p>
+            <a href="booking.php?movie=RMC"><button type="button" >Book Now</button></a>
+        </div>
+    </div>
+    <div class='card3D'>
+        <div><img width="160" height="270" src=lightyear-poster.jpg>
+            <h2>Lightyear - PG</h2>
+        </div>
+        <div>
+            <p>While spending years attempting to return home, marooned Space Ranger Buzz Lightyear encounters an army of ruthless robots commanded by Zurg who are attempting to steal his fuel source.</p>
+            <p>Mon-Tue: 12pm</p>
+            <p>Wed-Fri: 6pm</p>
+            <p>Sat-Sun: 12pm</p>
+            <a href="booking.php?movie=FAM"><button type="button" >Book Now</button></a>
+        </div>
+    </div>
+    <div class='card3D'>
+        <div><img width="160" height="270" src=prithviraj-poster.jpg>
+            <h2>Prithviraj - M</h2>
+        </div>
+        <div>
+            <p>A fearless warrior. An epic love story. Witness the grand saga of Samrat Prithviraj Chauhan.</p>
+            <p>Mon-Tue: 6pm</p>
+            <p>Sat-Sun: 9pm</p>
+            <a href="booking.php?movie=AHF"><button type="button" >Book Now</button></a>
+        </div>
+    </div>
+</div>
+
+</section>
+<br>
+<footer>
+<p>Jordan Kerever - s3462724 - Group B (Sama). My github repo: <a href="https://protect-au.mimecast.com/s/198SC6XQ88iZomY2C6l1q2?domain=github.com">https://protect-au.mimecast.com/s/198SC6XQ88iZomY2C6l1q2?domain=github.com</a></p>
+</footer>
+</main>
+</body>
 </html>
